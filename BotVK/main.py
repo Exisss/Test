8a4d5fafac543742ca328fac4aa36f7f54c822e8
Exisss <serioga14e@mail.ru>
2019-02-01 11:54:06 +0300

    Add files via upload

--- BotVK/main.py
+++ BotVK/main.py
@@ -10,12 +10,13 @@ vk_session = vk_api.VkApi(token=token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
+print()
 print('           ##############            ')
 print('        ##                ##         ')
-print('      ##                    ##       ')
-print('   ##                         ##     ')
-print('  #  ########################## #    ')
-print(' #   ##########################  #   ')
+print('      ##                     ##      ')
+print('   ##                          ##    ')
+print('  #  ########################### #   ')
+print(' #    #########################  #   ')
 print(' #     ###                ####   #   ')
 print(' #      ###              ###     #   ')
 print(' #       ###            ###      #   ')
@@ -26,18 +27,19 @@ print('   ##             ###         ##     ')
 print('    ##          ###         ##       ')
 print('      ##                  ##         ')
 print('         ################            ')
+print()
 
-
+print('============')
 print('Бот запущен. \nОбработка сообщений запущена.')
 while True:
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             #print('Сообщение получено' +str(datetime.strftime(datetime.now(), "%H:%M:%S")))
             #print('Текст: ' + event.text)
-            print('+================================================================================')
+            print('+===============================================================================')
             print('+ Сообщение получено в ' + str(datetime.strftime(datetime.now(),"%H:%M:%S")), 'пользователь https://vk.com/id', event.user_id)
             print('+ Текст: ' + str(event.text))
-            print('+================================================================================')
+            print('+===============================================================================')
             print()
             response = event.text.lower()
             if event.from_user and not (event.from_me):
@@ -47,6 +49,18 @@ while True:
                 elif response == "пока":
                     vk_session.method('messages.send',
                                       {'user_id': event.user_id, 'message': 'До свидания!', 'random_id': 0})
+                elif response == "инфо":
+                    vk_session.method('messages.send',
+                                      {'user_id': event.user_id, 'message': 'BotVK v20190201\n Интеллектуальная собственность Чибисова С.С.{https://vk.com/id48406925}', 'random_id': 0})
+
+                elif response == "help":
+                    vk_session.method('messages.send',
+                                      {'user_id': event.user_id, 'message': 'BotVK v20190201 \nКоманды: \n1) привет \n2) пока\n3) инфо - информация о боте', 'random_id': 0})
+
+                elif response == "@exiss":
+                    vk_session.method('messages.send',
+                                      {'user_id': event.user_id, 'message': 'Exiss, добро пожаловать!', 'random_id': 0})
+
                 #elif response == "КОТИКИ":
                 #    attachment = get_pictures.get(vk_session, -23530818, session_api)
                 #    vk_session.method('messages.send',
